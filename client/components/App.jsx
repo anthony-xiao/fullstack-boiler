b0aa57d5425b6e449bfc1853a60408e6f43fce55
anthony-xiao <juniorxiao01@gmail.com>
2018-07-07 00:51:00 +1200

    add in redux

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import {Route, Link} from 'react-router-dom'
 
 class App extends React.Component {
   render () {
     return (
-      <div>hi hi</div>
+      <div>
+        hi hi
+        <Link />
+        <Route />
+      </div>
     )
   }
 }
